1b00fd3361ebe104150877bd59fe93e4bfa2e0d5
BlueLight <dlrkdmf10@gmail.com>
2025-01-08 20:26:19 +0900

    ✨ Day2 CRUD
    day2 CRUD

--- src/users/request.py
+++ src/users/request.py
@@ -1,6 +1,17 @@
-from pydantic import BaseModel
 from datetime import date
+from pydantic import BaseModel, field_validator, ValidationError
 
-class CreateUserRequest(BaseModel):
+class UserCreateRequestBody(BaseModel):
     name: str
     date_of_birth: date # 생일
+
+    @field_validator("date_of_birth")
+    def validate_date_of_birth(cls, v):
+        age_delta = date.today() - v
+        if age_delta.days < 5 * 365:
+            raise ValueError("만 6세 이상만 가입할 수 있습니다.")
+        return v
+
+
+class UserUpdateRequestBody(BaseModel):
+    name: str
--- src/users/routers.py
+++ src/users/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from datetime import date
-from users.request import CreateUserRequestBody
+from users.request import UserCreateRequestBody, UserUpdateRequestBody
 
 router = APIRouter(tags=["Users"])
 
@@ -13,10 +13,39 @@ users = [
 def get_users_handler():
     return users
 
-# 새로운 유저 생성 API
+# C: 새로운 유저 생성 API
 @router.post("/users")
-def create_user_handler(body: CreateUserRequestBody):
+def create_user_handler(body: UserCreateRequestBody):
     # 1. 사용자로부터 데이터를 받고
     # 2. 받은 데이터의 유효성 검사
     # 3. 새로운 유저 데이터를 유저 목록에 추가
-    return
+    new_user = {
+        "id": len(users) + 1,
+        "name": body.name,
+        "date_of_birth": body.date_of_birth,
+    }
+    users.append(new_user)
+    return new_user
+
+# R: 상세 유저 조회 API
+@router.get("/users/{user_id}")
+def get_user_handler(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+
+# U: 유저 업데이트 API
+@router.patch("/users/{user_id}")
+def update_user_handler(user_id: int, body: UserUpdateRequestBody):
+    for user in users:
+        if user["id"] == user_id:
+            user["name"] = body.name
+            return user
+
+# D: 유저 삭제 API
+@router.delete("/users/{user_id}")
+def delete_user_handler(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            users.remove(user)
+    return users
